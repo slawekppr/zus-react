69fb83c752a158313d48887c904389ccac496b26
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-02 12:54:12 +0100

    tsc --strict // tsconfig - strict: true

--- trener/2_frontend/src/index.ts
+++ trener/2_frontend/src/index.ts
@@ -1,3 +1,9 @@
-console.log('Hello jQuery');
+console.log("Hello jQuery");
 
-$('#title').text('Hello jQuery')
+// $('#title').text('Hello jQuery')
+
+function add(x: any, y: any) {
+  return x + y;
+}
+
+document.getElementById("test")!.textContent = "123";
