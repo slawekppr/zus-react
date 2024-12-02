029c2ddc5b7b53d996aa82b56ce6a8386ea3dbca
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-02 11:35:53 +0100

    Exercise: Configure TS for web project

--- trener/1_migracja_z_js/src/main.ts
+++ trener/1_migracja_z_js/src/main.ts
@@ -1,11 +1,11 @@
 /// <reference path="./types.d.ts" />
-
 import { sub, div, mult } from "./lib.js";
 import jQuery from "jquery";
 
 // jQuery.placki;
 // document.getElementById('title')!.textContent = 'Hello TS'
 
+// const title: HTMLElement | null = document.getElementById("title"); // Union Type
 const title = document.getElementById("title"); // Union Type
 
 // Type Narrowing
