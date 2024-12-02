86fc1acfea88960b9c4f402d9489882ac9bf049b
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-02 15:30:57 +0100

    Exercise: declaration merging

--- trener/3_backend/src/index.ts
+++ trener/3_backend/src/index.ts
@@ -5,14 +5,26 @@ import type { User } from "./users";
 const app = express();
 
 app.use((req, res, next) => {
-    // TODO: Authorize
-    req.user = {name:'Admin'}
-    next()
+  // TODO: Authorize
+  req.user = { name: "Admin" };
+  next();
 });
 
 app.get("/", (req, res) => {
   res.send(`<h1>Hello ${req.user.name} | Guest!</h1>`);
 });
+
+declare global {
+    namespace Express {
+        // These open interfaces may be extended in an application-specific manner via declaration merging.
+        // See for example method-override.d.ts (https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/method-override/index.d.ts)
+        interface Request {
+            
+        }
+    }
+}
+
+// app.get("", (req, res) => {});
 
 app.get("/users", (req, res) => {
   const usersData = fs.readFileSync("./data/users.json", { encoding: "utf8" });
@@ -35,3 +47,5 @@ const PORT = Number(process.env["PORT"]) || 8080;
 app.listen(PORT, HOST, () => {
   console.log(`Listening on http://${HOST}:${PORT}`);
 });
+
+
