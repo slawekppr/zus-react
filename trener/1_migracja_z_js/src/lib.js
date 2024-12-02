bd5b57cbd9f1209a8ff4a7627bed23a41c731653
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-02 10:10:45 +0100

    JSDoc - types in JS

--- trener/1_migracja_z_js/src/lib.js
+++ trener/1_migracja_z_js/src/lib.js
@@ -1,3 +1,12 @@
+
+/**
+ * Best substraction ever!
+ * @param {number} x First number
+ * @param {number} y Second number
+ * @returns {number} result
+ */
 export function sub(x, y) {
   return x - y;
 }
+
+sub(1, '2');
--- trener/1_migracja_z_js/src/main.ts
+++ trener/1_migracja_z_js/src/main.ts
@@ -3,5 +3,6 @@ import { sub } from "./lib.js";
 const add = (x: number, y: number) => {
   return x + y;
 };
-sub(1, 2);
 add(1, 2);
+
+sub(1, '2');
