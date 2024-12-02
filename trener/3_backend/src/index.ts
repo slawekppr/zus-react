91356382d89d75ff13c72544c5ff55a7e4417bfa
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-02 15:21:45 +0100

    Narrowing

--- trener/3_backend/src/index.ts
+++ trener/3_backend/src/index.ts
@@ -15,28 +15,18 @@ app.get("/users", (req, res) => {
   const usersData = fs.readFileSync("./data/users.json", { encoding: "utf8" });
   const users: User[] = JSON.parse(usersData);
 
-  // localhost?name=   name.includes(...)
   const nameParam = req.query["name"];
-
   //   const name = nameParam as string;
   //   const name = String(nameParam);
 
   if (typeof nameParam === "string") {
-    // nameParam // string
     res.json(users.filter((u) => u.name.includes(nameParam)));
-  } else {
-    nameParam; //  QueryString.ParsedQs | string[] | QueryString.ParsedQs[] | undefined
+  } else if (typeof nameParam === "undefined") {
     res.json(users);
+  } else {
+    nameParam;
+    res.json({ message: "Bad param" });
   }
-  
-  res.json({ message: "Bad param" });
-
-  //   if (typeof nameParam !== "object") {
-  //     nameParam ?? "";
-  //     if (nameParam !== undefined) {
-  //       nameParam;
-  //     }
-  //   }
 });
 
 const HOST = process.env["HOST"] || "localhost";
