75940a3d359b962715df0d6d1b2f110b767efd01
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-02 13:09:48 +0100

    sourceMap

--- trener/2_frontend/src/index.ts
+++ trener/2_frontend/src/index.ts
@@ -6,4 +6,27 @@ function add(x: any, y: any) {
   return x + y;
 }
 
-document.getElementById("test")!.textContent = "123";
+// document.getElementById("test")!.textContent = "123";
+
+class Person {
+  // name:string
+
+//   #secret = "tajne";
+
+  constructor(public name: string) {
+    // this.name = name
+    debugger
+  }
+
+  probujemy(){
+    throw new Error('ups...')
+  }
+
+  get costam() {
+    return "";
+  }
+
+  static DRIVING_AGE = 18;
+}
+
+new Person('Baska!').probujemy()
