b5207e36e9664992f6887625626952b1138df971
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-02 10:15:05 +0100

    @ts-check // @ts-ignore // @ts-expect-error

--- trener/1_migracja_z_js/src/lib.js
+++ trener/1_migracja_z_js/src/lib.js
@@ -1,12 +1,19 @@
+// @ts-check
 
 /**
  * Best substraction ever!
  * @param {number} x First number
- * @param {number} y Second number
+ * @param {string} y Second number
  * @returns {number} result
  */
 export function sub(x, y) {
   return x - y;
 }
 
-sub(1, '2');
+sub(1, 1);
+
+// @ts-ignore
+sub(1, "2");
+
+// @ts-expect-error
+sub(1, "2");
