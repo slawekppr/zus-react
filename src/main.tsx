bca0214bbdc08818acc830236dac35a833897591
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-03 14:50:33 +0100

    React.createElement(PersonProfile, { user })

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.tsx";
-
-// Dev: expose on window to console:
-window.React = React;
-window.ReactDOM = ReactDOM;
-(window as any).ReactDOM = ReactDOM;
 
 const root = createRoot(document.getElementById("root")!);
 
@@ -63,55 +57,17 @@ const UsersList = ({ users }: { users: User[] }) =>
   React.createElement(
     "ul",
     null,
-    users.map(
-      (user) =>
-        React.createElement(
-          "li",
-          { key: user.id },
-          PersonProfile({
-            user,
-            children: React.createElement("button", {}, "Select"),
-          })
-        ) // Structuring
+    users.map((user) =>
+      // React.createElement("li", { key: user.id }, PersonProfile({ user }))
+      React.createElement(
+                                               // Suspeneded element
+        "li", { key: user.id }, React.createElement(PersonProfile, { user })
+      )
     )
   );
 
-root.render(UsersList({ users }));
+// const vdom = UsersList({ users }); // 3x render PersonProfile
 
-// ReactDOM.render(vdiv, root);
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+UsersList({ users }); //  PersonProfile not rendered yet
 
-// export {} // module
-
-// div = document.createElement('div')
-// p = document.createElement('p')
-// div.append(p)
-// p.innerText = 'Alice has a cat'
-// root.append(div)
-
-// user = '<h1>Alice Hxr PWd <script> <img> '
-// root.innerHTML = `
-//  <div><p>${user} has a <b>cat<b></p></div>
-// `
-
-// user = 'Alice '
-// root.innerHTML = `
-//  <div>
-//  <p>${user} has a <b>cats<b></p>
-//  <input>
-//  </div>
-
-// vdiv = React.createElement('div',{
-//   id:'placki 123',
-//   className:'user-card',
-//   style: { color:'red' }
-// },
-// React.createElement('p',null,'Ala ma kota'),
-// React.createElement('input')
-// React.createElement("input", { key: "tutaj jestem" })
-
-// )
+root.render(UsersList({ users })); // 3x render PersonProfile
