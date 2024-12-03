e2311961034ff115872190a70ba38202f495bb38
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-03 15:02:21 +0100

    const [color, setColor] = useState(user.color);

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -39,35 +39,49 @@ type Props = {
   children?: React.ReactNode;
 };
 
-const PersonProfile = ({ user, children }: Props) =>
-  React.createElement(
+const PersonProfile = ({ user, children }: Props) => {
+                            //  root.tutajjestem ?? 
+  const [color, setColor] = useState(user.color);
+
+  return React.createElement(
     "div",
     {
       id: `user_${user.id}`,
       className: "user-card",
-      style: { color: user.color, border: "1px solid" },
+      style: { color: color, border: "1px solid" },
     },
     user.pet // Conditional Rendering
       ? React.createElement("p", null, `${user.name} has a ${user.pet.name}`)
       : React.createElement("p", { style: {} }, `${user.name} has no pet`),
-    children
+    children,
+    React.createElement(
+      "button",
+      {
+        onClick() {
+          setColor(color == "red" ? user.color : "red"); // impure state + dirty
+        },
+      },
+      "Select"
+    )
   );
+};
 
 const UsersList = ({ users }: { users: User[] }) =>
   React.createElement(
     "ul",
     null,
     users.map((user) =>
-      // React.createElement("li", { key: user.id }, PersonProfile({ user }))
       React.createElement(
-                                               // Suspeneded element
-        "li", { key: user.id }, React.createElement(PersonProfile, { user })
+        "li",
+        { key: user.id },
+        // root.tutajjestem = tu
+        React.createElement(PersonProfile, { user })
       )
     )
   );
 
-// const vdom = UsersList({ users }); // 3x render PersonProfile
-
+// Pure
 UsersList({ users }); //  PersonProfile not rendered yet
 
+// Impure
 root.render(UsersList({ users })); // 3x render PersonProfile
