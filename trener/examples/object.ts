7016f2c1e8086b0b30d37dd8e8605b3e26cd7dee
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-03 09:05:54 +0100

    object vs Object vs {}

--- trener/examples/object.ts
+++ trener/examples/object.ts
@@ -1,12 +1,27 @@
-
 function showobject(params: object) {
-  params;
+  if ("x" in params) params.x;
 }
+// showobject(123) // Error
+showobject({});
+showobject({ x: 1 });
+showobject(new Date());
+// showobject(null);
+// showobject(undefined);
 
 function showObject(params: Object) {
-  params;
+  params.hasOwnProperty("x");
 }
+showObject(123);
+showObject({});
+showObject({ x: 1 });
+// showObject(null);
+// showObject(undefined);
 
 function showObj(params: {}) {
   params;
 }
+showObj(123);
+showObj({});
+showObj({ x: 1 });
+// showObj(null)
+// showObj(undefined)
