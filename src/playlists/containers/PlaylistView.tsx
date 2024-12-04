9e012bc0733f7f3c5cf81c8ba5aec882a344ee8d
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-04 13:02:06 +0100

    sWitching modes - Enum

--- src/playlists/containers/PlaylistView.tsx
+++ src/playlists/containers/PlaylistView.tsx
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import PlaylistList from "../components/PlaylistList";
 import PlaylistDetails from "../components/PlaylistDetails";
 import PlaylistEditor from "../components/PlaylistEditor";
 import { Button } from "primereact/button";
 // tsrafce
 
+enum MODES {
+  details = 'details',
+  editor = 'editor',
+}
+
 type Props = {};
 
 const PlaylistView = (props: Props) => {
-  const mode = "details";
+  const [mode, setMode] = useState<MODES>(MODES.details);
 
-  const showEditor = () => {};
-  const showDetails = () => {};
+  const showDetails = () => {
+    setMode(MODES.details);
+  };
+
+  const showEditor = () => {
+    setMode(MODES.editor);
+  };
 
   return (
     <div>
-      {/* Emmet + tailwind  */}
-      {/* .grid.grid-cols-2.gap-5>div.grid.gap-2*2 */}
       <div className="grid grid-cols-2 gap-5">
         <div className="grid gap-5">
           <PlaylistList />
         </div>
 
         <div className="grid gap-5">
-          {true  == true ? <PlaylistDetails /> : <PlaylistEditor />}
+          {mode == MODES.editor ? <PlaylistDetails /> : <PlaylistEditor />}
 
           <div className="flex justify-between">
-            <Button severity="warning">Detalils</Button>
-            <Button>Edit</Button>
+            <Button severity="warning" onClick={showDetails}>
+              Detalils
+            </Button>
+            <Button onClick={() => showEditor()}>Edit</Button>
           </div>
         </div>
       </div>
