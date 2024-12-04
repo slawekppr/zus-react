e5b69012e9ca20e2e5aea630726e515fe9831453
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-04 11:10:32 +0100

    value - controlled, VS defaultValue - uncontrolled

--- src/playlists/components/PlaylistEditor.tsx
+++ src/playlists/components/PlaylistEditor.tsx
@@ -12,23 +12,31 @@ const PlaylistEditor = (props: Props) => {
 
   return (
     <div>
-      {/* .grid.gap-5>.grid.gap-2*3>label{Name}+input */}
+
+      {/* 
+        Warning: You provided a `value` prop to a form field without an `onChange` handler. 
+        This will render a read-only field. 
+        If the field should be mutable use `defaultValue`. 
+        Otherwise, set either `onChange` or `readOnly`. 
+      */}
+      
       <div className="grid gap-5">
         <div className="grid gap-2">
           <label>Name</label>
-          <input type="text" />
+          <input type="text" value={playlist.name} />
+          <div className="text-end">{playlist.name.length} / 100</div>
         </div>
 
         <div className="grid gap-2">
           <label>
-            <input type="checkbox" />
+            <input type="checkbox" defaultChecked={playlist.public} />
             Public
           </label>
         </div>
 
         <div className="grid gap-2">
           <label>Description</label>
-          <textarea></textarea>
+          <textarea defaultValue={playlist.description}></textarea>
         </div>
       </div>
     </div>
