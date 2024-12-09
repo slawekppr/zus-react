6e3b9cb5878593065244db24bb56e0859a288908
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-09 10:36:21 +0100

    TypeGuard: function isAdmin(user: User): user is AdminUser {

--- src/common/model/Playlist.tsx
+++ src/common/model/Playlist.tsx
@@ -4,3 +4,24 @@ export interface Playlist {
   public: boolean;
   description: string;
 }
+
+type User = {
+  name: string;
+  isVerified: boolean;
+  isAdmin: boolean;
+};
+
+type VerifiedUser = User & { isVerified: true };
+type AdminUser = VerifiedUser & { isAdmin: true };
+
+const user: User = { name: "Admin", isAdmin: true, isVerified: true };
+
+function isAdmin(user: User): user is AdminUser {
+  return user.isAdmin === true;
+}
+
+if (isAdmin(user)) {
+  thisCanBeDoneByADminOnly(user);
+}
+
+function thisCanBeDoneByADminOnly(user: AdminUser) {}
