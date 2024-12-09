79239ab8e56f38d31b22aaa8d325823dcb8b4767
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-09 09:04:32 +0100

    Narrowing + Return Types (never/throw)

--- src/common/model/Playlist.tsx
+++ src/common/model/Playlist.tsx
@@ -5,9 +5,20 @@ export interface Playlist {
   description: string;
 }
 
-function normalizeId(id: number | string) {
+function normalizeId(id: number | string /* Union */) {
+  // return String(id) // Conversion
+  // return id.toString(); // intersection
+  // id.valueOf // Union - string | number
 
-  
+  if (typeof id === "number") {
+    return id.toFixed(0);
+  }
+  if (typeof id === "string") {
+    return id.toLowerCase();
+  }
 
-  // return id.toFixed(0);
+  id satisfies never; // never // TS
+  throw new Error("Invalid ID " + id); // JS
+
+  // return void() // undefined
 }
