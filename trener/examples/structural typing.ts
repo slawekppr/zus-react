7f3fbd2153a84225eb0b15b89af62791f3be88d2
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-09 11:13:55 +0100

    // Type Wideing  - if ("length" in p && typeof p.length === "number") {

--- trener/examples/structural typing.ts
+++ trener/examples/structural typing.ts
@@ -1,13 +1,25 @@
 interface Vector {
   x: number;
   y: number;
- length: number;
+  length: number;
 }
 type Point = { x: number; y: number };
 
 let v: Vector = { x: 423, y: 233, length: 123 };
 let p: Point = { x: 123, y: 123 };
 
-// Structural Typing ( vs. Nominal Typing ) a.k.a Duck Typing 
+// Structural Typing ( vs. Nominal Typing ) a.k.a Duck Typing
 p = v;
 // v = p; // Property 'length' is missing in type 'Point' but required in type 'Vector'.
+
+// Convariance - give at least what is expected
+
+p = v; // p and v points to same memory address. TypeScript "hides" extra data
+
+(p as Vector).length;
+
+// Type Wideing
+if ("length" in p && typeof p.length === "number") {
+  /// p ???
+  p.length;
+}
