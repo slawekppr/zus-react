79a25ee5d7d64c1ae876578d946e71b8d57b5f7d
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-09 13:53:40 +0100

    type Person = [id: number, name: string];

---
+++ trener/examples/array tuple.ts
@@ -0,0 +1,18 @@
+const tablica = [123, "Zbigniew", "Banan", true]; // (string | number | boolean)[]
+tablica[1] = false;
+tablica.push(123);
+
+const krotka = [123, "Zbigniew", "Banan", true] as const;
+
+let names: string[] = [];
+
+type Person = [id: number, name: string];
+
+let person: Person = [123, "Pankracy"];
+
+// function takePerson(person: Person) {}
+function takePerson(...person: Person) {}
+function takePerson2([id, name]: Person) { id; name }
+function savePerson(db: string, ...person: Person) {}
+
+takePerson(123, "123"); // takePerson(id: number, name: string)
