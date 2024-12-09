247eb622908af6432b5edb42ebfe5b927c742c0b
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-09 13:31:36 +0100

    Template Literal Types

--- trener/examples/string literals templates.ts
+++ trener/examples/string literals templates.ts
@@ -6,8 +6,37 @@ type ModesPlus = Modes | ModeThanString;
 let mode: ModesPlus = "details";
 
 type Header = "Authorization" | "Content-Type";
+type ExtraHeaders = `X-${string}`;
+type ExtendedHeaders = Header | ExtraHeaders;
 
-function setHeader(header: Header | (string & {})) {}
+// function setHeader(header: ExtendedHeaders | (string & {})) {}
+function setHeader(header: ExtendedHeaders) {}
 
+setHeader("X-Proxy");
+setHeader("X-Banaba");
+// setHeader("asd");
 setHeader("Authorization");
-setHeader("Cookies");
+// setHeader("Cookies");
+
+// Template Literal Types
+// const placki = `${123} + ${"asd" + "asd"}`;
+
+type protocols = "http" | "https";
+type domains = "zus.pl" | "zus.ad";
+
+/**
+ * Usage 1
+ * @param url
+ */
+function buildUrl(url: `${protocols}://${domains}/`);
+/**
+ * Usage 2
+ * @param url
+ */
+function buildUrl(url: `${protocols}://${domains}/${string}`, param: any);
+function buildUrl(url: string, param?: any) {
+  // if(param?)
+}
+
+buildUrl("https://zus.pl/");
+buildUrl("https://zus.pl/123", 123);
