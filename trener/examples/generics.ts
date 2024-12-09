44580fa9826fd4efe5e25839f1299055961e07bf
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-09 14:51:25 +0100

    Useful generics // function identity<T>(x: T): T { return x; }

--- trener/examples/generics.ts
+++ trener/examples/generics.ts
@@ -16,9 +16,12 @@ type TypedBox<T> = { value: T };
 const typedBox: TypedBox<string> = { value };
 typedBox.value;
 
-// Useless Generics 
+// Useless Generics
 declare function parseJSON<T>(json: string): T;
 declare function serializeJSON<T>(obj: T): string;
 
-JSON.parse('...') as 'Cokolwiek'
-serializeJSON({x:1}) === '???'
+JSON.parse("...") as "Cokolwiek";
+serializeJSON({ x: 1 }) === "???";
+
+// Useful generics
+function identity<T>(x: T): T { return x; }
