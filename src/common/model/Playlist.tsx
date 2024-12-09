27df93cc653065a31144a17d087ed825f888e315
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-09 09:49:14 +0100

    // ADT - Abstract Data Types

--- src/common/model/Playlist.tsx
+++ src/common/model/Playlist.tsx
@@ -4,3 +4,15 @@ export interface Playlist {
   public: boolean;
   description: string;
 }
+
+// ADT - Abstract Data Types
+
+type Idle = { isLoading: false };
+type Progress = { isLoading: true };
+
+type Success<T = unknown> = { data: T; error: never };
+type Failed<E = unknown> = { data: never; error: E };
+
+type Loaded<T, E> = Success<T> | Failed<E>;
+
+type RequestStatus<T, E> = Progress | Loaded<T, E> | Idle;
