eb3b40511f1d30d02c293860250373cd26d1888a
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-10 09:06:54 +0100

    InstanceType vs ConstructorType

--- trener/examples/typeof_keyof.ts
+++ trener/examples/typeof_keyof.ts
@@ -23,19 +23,40 @@ const UserRoles = {
 //'UserRoles' refers to a value, but is being used as a type here.
 // Did you mean 'typeof UserRoles'?
 
+// Typeof
 typeof "banana" === "string"; // JS
 
 type Roles4 = typeof UserRoles; // TS
 
 type Roles5 = Roles4["ADMIN"] | Roles4["MODERATOR"] | Roles4["USER"];
 
-type Roles5_5 = Roles4['ADMIN' | 'MODERATOR' | 'USER']
+type Roles5_5 = Roles4["ADMIN" | "MODERATOR" | "USER"];
 
+// Keyof
 type Roles6 = keyof Roles4;
 
 type Roles7 = keyof typeof UserRoles;
 
-type Roles8 = Roles4[ (keyof typeof UserRoles) ]
+type Roles8 = Roles4[keyof typeof UserRoles];
 
-type Roles9 = typeof UserRoles[ (keyof typeof UserRoles) ]
+// typeof keyof lookup[]
+type Roles9 = (typeof UserRoles)[keyof typeof UserRoles];
 
+const role: Roles9 = "admin";
+
+// typeof and classes
+
+class Placek {
+  sos = "bananowy";
+}
+
+const jedenPlacek = new Placek(); // Placek
+
+type InstanceType = typeof jedenPlacek; // Placek
+type ConstructorType = typeof Placek; // Placek
+
+function podajSniadanie(co: InstanceType) {}
+podajSniadanie(jedenPlacek)
+
+function zrobSniadanie( coZrobic: ConstructorType) {}
+zrobSniadanie( Placek )
