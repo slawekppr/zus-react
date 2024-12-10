8bc38685baf1888bc5280bec5b95ed6816d39bc4
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-10 09:40:55 +0100

    // Mapped Type With lookup   ( for e in ... dict[e]  )

--- trener/examples/indexed types.ts
+++ trener/examples/indexed types.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 export {};
 // ES6
 let klucz = 123;
@@ -45,4 +47,16 @@ req.headers["Accept"] = "banana";
 req.headers["X-Proxy"] = "banana";
 // req.headers["Proxy"] = "banana"; // errror
 
+type events = "click" | "change"; //| 'blur'
+type domEvents = `on${events}`;
+type reactEvents = `on${Capitalize<events>}`;
 
+type EventTypes = {
+  onClick: React.MouseEvent;
+  onChange: React.ChangeEvent;
+};
+
+// Mapped Type With lookup   ( for e in ... dict[e]  )
+type componentEventProps = {
+  [e in reactEvents]: React.EventHandler<EventTypes[e]>;
+};
