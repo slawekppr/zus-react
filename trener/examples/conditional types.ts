396ab3a15841b9bba903a3817ac1eacf9871a87b
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-10 11:18:27 +0100

    Conditional types on Unions

--- trener/examples/conditional types.ts
+++ trener/examples/conditional types.ts
@@ -6,8 +6,12 @@ type Point = { x: number; y: number };
 type Vector = { x: number; y: number; length: number };
 
 // wynik = warunek ? prawda : falsz
-type IsPointAVector = Point extends Vector ? "banana" : "placki";
-type IsVectorAPoint = Vector extends Point ? true : false;
+type IsPointAVector = Point extends Vector ? "banana" : "placki"; // placki
+type IsVectorAPoint = Vector extends Point ? true : false; // true
 
-// wynik = a >= b ? prawda : falsz
-type IsPointAPoint = Point extends Point ? 1 : 0;
+// wynik = a <= b ? prawda : falsz
+type IsPointAPoint = Point extends Point ? 1 : 0; // 1
+
+// Conditional types on Unions
+type ExtendsUnions1 = "banana" | "placki" extends string ? true : false;
+type ExtendsUnions2 = "banana" extends "banana" | "placki" ? true : false;
