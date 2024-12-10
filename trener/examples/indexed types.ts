7f48b353184231d8f988483c1a3f2bfa75a06439
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-10 09:25:39 +0100

    Exercise: Fix Headers MAP - indexed type

--- trener/examples/indexed types.ts
+++ trener/examples/indexed types.ts
@@ -8,20 +8,33 @@ const cachez = {
 cachez[klucz + "$"];
 // 'costam'
 
-
 /// --- tS ---
 
 type CacheItem = {
   name: string;
 };
 
-// Indexed Type 
-type Cache = { [klucz: string]: CacheItem; };
+// Indexed Type
+type Cache = { [klucz: string]: CacheItem };
+type SafeCache = { [klucz: string]: CacheItem | undefined };
 
-const cache: Cache = {
+const cache: SafeCache = {
   "123": { name: "playlist 123" },
   "345": { name: "playlist 345" },
 };
 cache["123"] = { name: "" };
 cache["234"] = { name: "" };
+// cache["does not exit!"].name.toUpperCase();
 
+// Headers MAP
+type RequestConfig = {
+  headers: {};
+};
+const req: RequestConfig = {
+  headers: {
+    Accept: "",
+    Authorization: "",
+  },
+};
+
+req.headers["X-PRoxty"] = "banana";
