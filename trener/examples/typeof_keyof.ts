1390b591965b1d0eedf892b1de12f1878ba3cfdd
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-10 08:43:29 +0100

    JS typeof != TS typeof

--- trener/examples/typeof_keyof.ts
+++ trener/examples/typeof_keyof.ts
@@ -1,15 +1,28 @@
 export {};
 
 type User = { role: "user" };
-type Moderator = { role: "moderator", isModerator:true };
-type Administrator = { role: "administrator", isAdmin:true };
+type Moderator = { role: "moderator"; isModerator: true };
+type Administrator = { role: "administrator"; isAdmin: true };
 
 type Role1 = "user" | "moderator" | "administrator";
 type UserRole = { role: Role1 };
 
 type Role2 = User["role"] | Moderator["role"] | Administrator["role"];
 
-type Users = User | Moderator | Administrator
-type Role3 = Users['role']
+type Users = User | Moderator | Administrator;
+type Role3 = Users["role"];
 
-type Role = (User | Moderator | Administrator)['role']
+type Role = (User | Moderator | Administrator)["role"];
+
+const UserRoles = {
+  USER: "user",
+  ADMIN: "admin",
+  MODERATOR: "moderator",
+}; // as const;
+
+//'UserRoles' refers to a value, but is being used as a type here.
+// Did you mean 'typeof UserRoles'?
+
+typeof "banana" === "string"; // JS
+
+type Roles4 = typeof UserRoles; // TS
