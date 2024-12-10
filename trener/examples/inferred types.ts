1abdf668ecd8a4146f951447b68939e5c5c0c170
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-10 13:21:23 +0100

    Inferred Types - extends (x: infer T) => any ? T : false;

--- trener/examples/inferred types.ts
+++ trener/examples/inferred types.ts
@@ -1,1 +1,25 @@
+export {};
 
+type Wrapper<T> = {
+  value: T;
+};
+
+function getValue<T>(param: Wrapper<Wrapper<T>>) {
+  return param.value.value;
+}
+
+// function drawLine(p1: { x: number, y:number }, p2: { x: number }) {}
+
+// Exercise: Extract  GeolocationPosition from geolocation API
+// const coords: GeolocationPosition[]
+// not exported GeolocationPosition
+
+window.navigator.geolocation.getCurrentPosition((res) => {
+  res.coords;
+});
+
+type PosFn = (typeof window)["navigator"]["geolocation"]["getCurrentPosition"];
+
+type ExtractGeoPos = PosFn extends (x: infer T) => any ? T : false;
+
+const placki: ExtractGeoPos = {} as PositionCallback;
