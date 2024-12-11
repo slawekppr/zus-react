5bde04addafd5932b185cf86d25a4d94fc61d508
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-11 08:47:45 +0100

    // Functional Combinator - isNot (aka. complement )

--- trener/examples/functional.ts
+++ trener/examples/functional.ts
@@ -1,8 +1,12 @@
+#!/usr/bin/env tsx
+
+// npx tsx ./trener/examples/functional.ts
+
 {
   const list = [1, 2, 3, 4];
   const list2 = [];
 
-  for (let l of list) console.log(l);
+  //   for (let l of list) console.log(l);
 
   const isEven = (x: number) => x % 2 == 0;
   const multBy = (factor: number) => (x: number) => x * factor;
@@ -15,15 +19,31 @@
     .map(multBy(2))
     .forEach(show);
 }
-// Exercise: Write 'isNot' function:
 
-const list = [1, 2, 3, 4];
+import * as R from "remeda";
 
-const isNot =  ?????; // FIXME!
+{
+  // Exercise: Write 'isNot' function:
 
-const isEven = (x: number) => x % 2 === 0;
-// const isOdd = (x: number) => x % 2 !== 0;
-const isOdd = isNot(isEven)
+  const list = [1, 2, 3, 4];
 
+  // Functional Combinator - isNot (aka. complement )
+  //   const isNot = (fn: (x: number) => boolean) => {
+  //     return (x: number) => {
+  //       return !fn(x);
+  //     };
+  //   };
+  // const isNot = fn => x => !fn(x)
 
-list.filter(isEven);
+  const isNot = (fn: (x: number) => boolean) => (x: number) => !fn(x);
+
+  const isEven = (x: number) => x % 2 === 0;
+  // const isOdd = (x: number) => x % 2 !== 0;
+
+  const isOdd = isNot(isEven);
+
+  // const result = list.filter(isEven);
+  const result = list.filter(isOdd);
+
+  console.log(result);
+}
