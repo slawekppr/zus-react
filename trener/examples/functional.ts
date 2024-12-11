16bbf5d40064d0adf67e73bb78cefe9829ff11b3
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-11 08:24:47 +0100

    Exercise: Write missing 'isNot' function

--- trener/examples/functional.ts
+++ trener/examples/functional.ts
@@ -1,15 +1,29 @@
+{
+  const list = [1, 2, 3, 4];
+  const list2 = [];
+
+  for (let l of list) console.log(l);
+
+  const isEven = (x: number) => x % 2 == 0;
+  const multBy = (factor: number) => (x: number) => x * factor;
+  const show = (x: number) => console.log(x);
+
+  const res = list
+    .filter(isEven)
+    //   .map((x) => multBy(2, x))
+    //   .map((x) => multBy(2) (x))
+    .map(multBy(2))
+    .forEach(show);
+}
+// Exercise: Write 'isNot' function:
+
 const list = [1, 2, 3, 4];
-const list2 = [];
 
-for (let l of list) console.log(l);
+const isNot =  ?????; // FIXME!
 
-const isEven = (x: number) => x % 2 == 0;
-const multBy = (factor: number) => (x: number) => x * factor;
-const show = (x: number) => console.log(x);
+const isEven = (x: number) => x % 2 === 0;
+// const isOdd = (x: number) => x % 2 !== 0;
+const isOdd = isNot(isEven)
 
-const res = list
-  .filter(isEven)
-  //   .map((x) => multBy(2, x))
-  //   .map((x) => multBy(2) (x))
-  .map(multBy(2))
-  .forEach(show);
+
+list.filter(isEven);
