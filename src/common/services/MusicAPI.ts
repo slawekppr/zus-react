3b2e50672594e6ef3eff49275104ec8468846092
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-11 11:52:27 +0100

    Exercise: Use mock api to search for Cats ;-)

--- src/common/services/MusicAPI.ts
+++ src/common/services/MusicAPI.ts
@@ -1,5 +1,7 @@
 import { mockAlbums } from "../fixtures/mockAlbums";
 
 export const fetchAlbumSearchResults = (query = "") => {
+  console.log("Searching ", query);
+
   return Promise.resolve(mockAlbums);
 };
