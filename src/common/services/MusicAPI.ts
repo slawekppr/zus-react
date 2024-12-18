4cf2c6b4832dfb8057884311286a67a0e7ebabfd
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-18 11:51:36 +0100

    ky.create()  ..extend(..)

--- src/common/services/MusicAPI.ts
+++ src/common/services/MusicAPI.ts
@@ -1,50 +1,44 @@
 import ky from "ky";
 import { mockAlbums } from "../fixtures/mockAlbums";
-import { Album, AlbumSearchResponse } from "../model/Album";
+import {
+  Album,
+  AlbumResponse,
+  AlbumSearchResponse,
+  PagingObject,
+} from "../model/Album";
 import { getToken } from "./Auth";
+import { Playlist } from "../model/Playlist";
 
-// axios $.getJSON $.ajax
+const MusicAPI = ky.create({
+  prefixUrl: "https://api.spotify.com/v1/",
+  headers: {
+    Authorization: `Bearer ${getToken()}`,
+  },
+});
+const albumsAPI = MusicAPI.extend((parent) => ({
+  prefixUrl: `${parent.prefixUrl}/albums`,
+}));
 
 export const fetchAlbumSearchResults = (query = "", init?: RequestInit) => {
-  const req = ky.get<AlbumSearchResponse>("https://api.spotify.com/v1/search", {
+  return MusicAPI.get<AlbumSearchResponse>("search", {
     searchParams: {
       type: "album",
       q: query,
     },
-    headers: {
-      Authorization: `Bearer ${getToken()}`,
-    },
     ...init,
-  }).json() // Promise waiting for Response Promise and then Body JSON Promise
-
-  return req.then((r) => r.json()).then((d) => d.albums.items as Album[]);
-};
-
-export const fetchAlbumSearchResults2 = (query = "", init?: RequestInit) => {
-  return fetch(
-    "https://api.spotify.com/v1/search?" +
-      new URLSearchParams({
-        type: "album",
-        q: query,
-      }),
-    {
-      ...init,
-      headers: {
-        ...init?.headers,
-        Authorization: `Bearer ${getToken()}`,
-      },
-    }
-  )
-    .then((r) => (r.ok ? r.json() : Promise.reject(new Error("Server Error"))))
-    .then((d) => d.albums.items as Album[]);
+  })
+    .json()
+    .then((d) => d.albums.items);
 };
 
 export const fetchAlbumById = (id = "", init?: RequestInit) => {
-  return fetch("https://api.spotify.com/v1/albums/" + id, {
-    ...init,
-    headers: {
-      ...init?.headers,
-      Authorization: `Bearer ${getToken()}`,
-    },
-  });
+  return albumsAPI.get<AlbumResponse>(id, init);
+};
+
+export const fetchPlaylistById = (id = "", init?: RequestInit) => {
+  return MusicAPI.get<Playlist>("playlists/" + id, init);
+};
+
+export const fetchMyPlaylists = (id = "", init?: RequestInit) => {
+  return MusicAPI.get<PagingObject<Playlist>>("/me/playlists/", init);
 };
