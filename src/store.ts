9b582e3363730edc6fb0f79fdbc3f9f6216aff63
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-19 15:45:30 +0100

    Playlist Slice, app hooks

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "./store/counter";
+import { playlistsSlice } from "./store/playlists";
 
 export const store = configureStore({
   reducer: {
     [counterSlice.name]: counterSlice.reducer,
+    [playlistsSlice.name]: playlistsSlice.reducer,
   },
 });
 
 (window as any).store = store;
 
-
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
+
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+import { useDispatch, useSelector } from 'react-redux'
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
---
+++ src/store/playlists.ts
@@ -0,0 +1,58 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Playlist } from "../common/model/Playlist";
+import { updateItem } from "../playlists/containers/utils";
+type Modes = "details" | "editor" | "creator";
+
+type State = {
+  mode: Modes;
+  items: Playlist[];
+  error?: Error;
+  selectedId?: Playlist["id"];
+};
+
+export const initialState: State = {
+  mode: "details",
+  items: [],
+};
+
+export const playlistsSlice = createSlice({
+  name: "playlists",
+  initialState,
+  reducers: {
+    Select(state, action: PayloadAction<Playlist["id"]>) {
+      state.selectedId = action.payload;
+    },
+    ShowCreator(state) {
+      state.mode = "creator";
+    },
+    ShowEditor(state) {
+      state.mode = "editor";
+    },
+    Cancel(state) {
+      state.mode = "details";
+    },
+    LoadPlaylists(
+      state,
+      action: PayloadAction<
+        { data: Playlist[]; error?: never } | { error: Error; data?: never }
+      >
+    ) {
+      if (action.payload.error) {
+        state.error = action.payload.error;
+      } else {
+        state.items = action.payload.data;
+      }
+    },
+    SavePlaylist(state, action: PayloadAction<Playlist>) {
+      state.mode = "details";
+      state.selectedId = action.payload.id;
+
+      state.items = updateItem(action.payload)(state.items);
+    },
+    CreatePlaylist(state, action: PayloadAction<Playlist>) {
+      state.mode = "details";
+      state.selectedId = action.payload.id;
+      state.items.push(action.payload);
+    },
+  },
+});
