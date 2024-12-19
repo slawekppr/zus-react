592d2a42e5674f317367229ce21631da50560851
Mateusz Kulesza <ev45ive@gmail.com>
2024-12-19 09:19:21 +0100

    React.memo // propsAreEqual

--- src/playlists/components/PlaylistList.tsx
+++ src/playlists/components/PlaylistList.tsx
@@ -9,26 +9,33 @@ type Props = {
 };
 
 // Controlled (state)
-const PlaylistList = ({ playlists, onSelect, selectedId }: Props) => {
-  return (
-    <div>
-      <div className="divide-y divide-slate-300 divide-solid">
-        {playlists.map((playlist, i) => (
-          <div
-            className={`p-4 ${
-              selectedId === playlist.id
-                ? "bg-pink-500 text-white"
-                : "cursor-pointer hover:bg-pink-200"
-            }`}
-            onClick={() => onSelect(playlist.id)}
-            key={playlist.id}
-          >
-            {i + 1}. {playlist.name}
-          </div>
-        ))}
+const PlaylistList = React.memo(
+  ({ playlists, onSelect, selectedId }: Props) => {
+    console.log("Reander List");
+
+    return (
+      <div>
+        <div className="divide-y divide-slate-300 divide-solid">
+          {playlists.map((playlist, i) => (
+            <div
+              className={`p-4 ${
+                selectedId === playlist.id
+                  ? "bg-pink-500 text-white"
+                  : "cursor-pointer hover:bg-pink-200"
+              }`}
+              onClick={() => onSelect(playlist.id)}
+              key={playlist.id}
+            >
+              {i + 1}. {playlist.name}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }, // propsAreEqual?:
+  (prevProps: Readonly<Props>, nextProps: Readonly<Props>) =>
+    prevProps.playlists === nextProps.playlists &&
+    prevProps.selectedId === nextProps.selectedId
+);
 
 export default PlaylistList;
